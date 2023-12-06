63cddc357bf81ae9c9e29dafb2d5873379c7702d
Qsen02 <diyangenchev73@gmail.com>
2023-12-06 15:35:19 +0200

    Малка промяна в кода

--- Calculator/src/server.js
+++ Calculator/src/server.js
@@ -22,17 +22,16 @@ function standartCalculatorLoad(body) {
     return template.replace('%%body%%', body);
 }
 app.get("/", (req, res) => {
-    console.log(req.params);
     let firstPage = indexPageLoad('<div class="container"><div class="content"><h1>Добре дошли!</h1></div><div class="content"><p>Изберете калкулатора, който искате да използвате:</p></div><div class="button"><button class="button" type="button"><a href="binaryCalculator.html">Двоичен калкулатор</a></button></div><div class="button"><button class="button" type="button"><a href="standartCalculator.html">Стандартен калкулатор</a></button></div></div>');
     res.send(firstPage);
 });
 app.get("/binaryCalculator.html", (req, res) => {
-    console.log("Потребител е влязъл в: двоичен калкулатор");
+    console.log(`Потребител е посетил: двоичен калкулатор`);
     let secondPage = binaryCalculatorLoad('<div class="container"><h1>Двоичен калкулатор</h1><div class="content"><label>Въведете двоичното число:</label><input class="content-input" type="text" id="binaryNum"></div><div class="content"><label>Десетично число:</label><input class="content-input" type="text" id="result2"></div><div class="content"><button type="button" id="convert">Конвертирай</button></div><script src="/public/js/binaryCalculator.js"></script></div>');
     res.send(secondPage);
 });
 app.get("/standartCalculator.html", (req, res) => {
-    console.log("Потребител е влязъл в: стандартен калкулатор");
+    console.log(`Потребител е посетил: стандартен калкулатор`);
     let thirdPage = standartCalculatorLoad('<div class="container"><h1>Калкулатор</h1><form><div class="content"><label class="form-label">Въведете първото число:</label><input class="form-input" type="number" id="a"></div><div class="content"><label>Въведете оператор:</label><input class="content-input" type="text" id="operator"></div><div class="content"><label>Въведете второто число:</label><input class="content-input" type="number" id="b"></div><div class="content"><label>Резултат:</label><input class="content-input" type="text" id="result1"></div><div class="content"><button type="button" id="calc">Изчисли</button></div><script src="/public/js/standartCalculator.js"></script></form></div>');
     res.send(thirdPage);
 });
