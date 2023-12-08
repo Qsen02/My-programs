54c75bd7a1d18df1cc4fe1fa9fc390f3ebe6a9f9
Qsen02 <diyangenchev73@gmail.com>
2023-12-08 11:58:28 +0200

    Добавен бутон за информация

---
+++ Calculator/public/js/info.js
@@ -0,0 +1,6 @@
+document.getElementById("binaryCalc").onclick = function() {
+    document.getElementById("answer").value = "Двоичния калкулатор приема двоично число и го конвертира в десетично, като имате предвид че двоичните числа съдържат само 0 и 1.";
+}
+document.getElementById("standartCalc").onclick = function() {
+    document.getElementById("answer").value = "Стандартния калкулатор извършва 5 вида операции между две числа и те са: събиране, изваждане , умножение, деление и степенуване като въвеждате първото число след това оператор(ето така:+,-,*,/,^) и след това второто число.";
+}
--- Calculator/src/server.js
+++ Calculator/src/server.js
@@ -21,8 +21,13 @@ function standartCalculatorLoad(body) {
     let template = fs.readFileSync('./public/standartCalculator.html').toString();
     return template.replace('%%body%%', body);
 }
+
+function infoLoad(body) {
+    let template = fs.readFileSync('./public/info.html').toString();
+    return template.replace('%%body%%', body);
+}
 app.get("/", (req, res) => {
-    let firstPage = indexPageLoad('<div class="container"><div class="content"><h1>Добре дошли!</h1></div><div class="content"><p>Изберете калкулатора, който искате да използвате:</p></div><div class="button"><button class="button" type="button"><a href="binaryCalculator.html">Двоичен калкулатор</a></button></div><div class="button"><button class="button" type="button"><a href="standartCalculator.html">Стандартен калкулатор</a></button></div></div>');
+    let firstPage = indexPageLoad('<div class="container"><div class="content"><h1>Добре дошли!</h1></div><div class="content"><p>Изберете калкулатора, който искате да използвате:</p></div><div class="button"><button class="button" type="button"><a href="binaryCalculator.html">Двоичен калкулатор</a></button></div><div class="button"><button class="button" type="button"><a href="standartCalculator.html">Стандартен калкулатор</a></button></div><div class="content"><button type="button" id="info"><a href="info.html">?</a></button></div></div>');
     res.send(firstPage);
 });
 app.get("/binaryCalculator.html", (req, res) => {
@@ -34,5 +39,10 @@ app.get("/standartCalculator.html", (req, res) => {
     console.log(`Потребител е посетил: стандартен калкулатор`);
     let thirdPage = standartCalculatorLoad('<div class="container"><h1>Калкулатор</h1><form><div class="content"><label class="form-label">Въведете първото число:</label><input class="form-input" type="number" id="a"></div><div class="content"><label>Въведете оператор:</label><input class="content-input" type="text" id="operator"></div><div class="content"><label>Въведете второто число:</label><input class="content-input" type="number" id="b"></div><div class="content"><label>Резултат:</label><input class="content-input" type="text" id="result1"></div><div class="content"><button type="button" id="calc">Изчисли</button></div><script src="/public/js/standartCalculator.js"></script></form></div>');
     res.send(thirdPage);
+});
+app.get("/info.html", (req, res) => {
+    console.log("Потребител е влязъл в информация");
+    let infoPage = infoLoad('<div class="container"><div class="content"><h1>Здравейте!<h1></div><div class="content"><p>Тук ще намерите информация за двата калкулатора.</p></div><div class="button" ><button type="button" id="binaryCalc">Как да използвам двоичен калкулатор?</button></div><div class="button" ><button type="button" id="standartCalc">Как да използвам стандартен калкулатор?</button></div><script src="/public/js/info.js"></script><div class="content"><input type="text" id="answer"></div></div>');
+    res.send(infoPage);
 });
 app.listen(3000);
