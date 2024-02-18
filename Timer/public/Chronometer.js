97dd69266955c55c28015d5d89549a6810bf27b1
Qsen02 <diyangenchev73@gmail.com>
2024-02-18 15:45:17 +0200

    update

--- Timer/public/Chronometer.js
+++ Timer/public/Chronometer.js
@@ -1,10 +1,14 @@
-document.getElementById("start").onclick = function() {
-    let breakPoint = Number(document.getElementById("breakPoint").value)
+document.getElementById("start").addEventListener("click", startCounting);
+
+function startCounting() {
+    let inputBreakpoint = document.getElementById("breakPoint");
+    let breakPoint = Number(inputBreakpoint.value);
     let hundreds = 0;
     let flag = false;
     if (breakPoint == 0) {
         breakPoint = 60;
     }
+    inputBreakpoint.value = "";
     for (let i = 0; i <= breakPoint; i++) {
         for (let j = 0; j < 100; j++) {
             setTimeout(() => document.getElementById("output").value = `${i} : ${j}`, hundreds);
@@ -19,6 +23,8 @@ document.getElementById("start").onclick = function() {
         }
     }
 }
-document.getElementById("reset").onclick = function() {
+document.getElementById("reset").addEventListener("click", zeroed);
+
+function zeroed() {
     document.getElementById("output").value = `00 : 00`;
 }
--- Timer/public/Timer.js
+++ Timer/public/Timer.js
@@ -1,6 +1,10 @@
-document.getElementById("start").onclick = function() {
-    let breakPoint = Number(document.getElementById("breakPoint").value)
+document.getElementById("start").addEventListener("click", startCounting);
+
+function startCounting() {
+    let inputBreakpoint = document.getElementById("breakPoint");
+    let breakPoint = Number(inputBreakpoint.value);
     let hundreds = 0;
+    inputBreakpoint.value = "";
     for (let i = breakPoint - 1; i >= 0; i--) {
         for (let j = 99; j >= 0; j--) {
             setTimeout(() => document.getElementById("output").value = `${i} : ${j}`, hundreds);
