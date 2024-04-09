68c0ee484c60b2afa0a328608f7b0f8b756ab333
Qsen02 <diyangenchev73@gmail.com>
2024-04-09 21:42:55 +0300

    update

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,54 +1,54 @@
 window.addEventListener("load", start);
 
 function start() {
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
     let squares = document.querySelectorAll(".content");
-    let squareArray = Array.from(squares)
+    let orinalgBoard = Array.from(Array(9).keys());
+    let squareArray = Array.from(squares);
     squareArray.forEach(el => el.addEventListener("click", fill));
     let buttonX = document.getElementById("X");
     let buttonO = document.getElementById("O");
-    let filler = "X";
     buttonX.classList.add("active");
     buttonX.addEventListener("click", changeX);
     buttonO.addEventListener("click", changeO);
+    let player = "X";
+    let bot = "O";
 
     function changeX() {
         buttonX.classList.add("active");
         buttonO.classList.remove("active");
-        filler = "X"
+        player = "X";
+        bot = "O";
     }
 
     function changeO() {
         buttonX.classList.remove("active");
         buttonO.classList.add("active");
-        filler = "O";
+        player = "O";
+        bot = "X";
     }
 
 
     function fill(event) {
-        event.target.textContent = filler;
-        checkForWin(filler);
+        const divId = event.target.dataset.id;
+        orinalgBoard[divId] = player;
+        event.target.textContent = player;
+        checkForWin(player);
         buttonX.disabled = true;
         buttonO.disabled = true;
-        enemyFill();
-    }
-
-    function enemyFill() {
-        let enemyFiller = changeFiller();
-        let index = Math.floor(Math.random() * 8);
-        while (squareArray[index].textContent != "") {
-            let count = 0
-            for (let square of squareArray) {
-                if (square.textContent == "") {
-                    count++;
-                }
-            }
-            if (count == 0) {
-                return;
-            }
-            index = Math.floor(Math.random() * 8);
-        }
-        setInterval(() => squareArray[index].textContent = enemyFiller, 1000);
-        setInterval(() => checkForWin(enemyFiller), 2000);
+        let botMove = minimax(orinalgBoard, bot).index
+        orinalgBoard[botMove] = bot;
+        setInterval(() => squareArray[botMove].textContent = bot, 500);
+        setInterval(() => checkForWin(bot), 1000);
     }
 
     function checkForWin(filler) {
@@ -99,13 +99,78 @@ function start() {
         divEl.appendChild(newGameBtn);
         bodyRef.appendChild(divEl);
         newGameBtn.addEventListener("click", () => {
-            buttonX.disabled = true;
-            buttonO.disabled = true;
+            buttonX.disabled = false;
+            buttonO.disabled = false;
             location.reload();
         })
     }
 
-    function changeFiller() {
-        return filler == "X" ? "O" : "X";
+    function emptySquares() {
+        return orinalgBoard.filter(el => typeof(el) === "number");
+    }
+
+    function checkWin(board, player) {
+        let plays = board.reduce((a, e, i) =>
+            (e === player) ? a.concat(i) : a, []);
+        let gameWon = null;
+        for (let [index, win] of winCombos.entries()) {
+            if (win.every(elem => plays.indexOf(elem) > -1)) {
+                gameWon = { index: index, player: player };
+                break;
+            }
+        }
+        return gameWon;
+    }
+
+    function minimax(newBoard, curPlayer) {
+        var availSpots = emptySquares();
+        console.log(newBoard);
+        console.log(availSpots);
+
+        if (checkWin(newBoard, player)) {
+            return { score: -10 };
+        } else if (checkWin(newBoard, bot)) {
+            return { score: 10 };
+        } else if (availSpots.length === 0) {
+            return { score: 0 };
+        }
+        var moves = [];
+        for (let i = 0; i < availSpots.length; i++) {
+            var move = {};
+            move.index = newBoard[availSpots[i]];
+            newBoard[availSpots[i]] = curPlayer;
+
+            if (curPlayer == bot) {
+                var result = minimax(newBoard, player);
+                move.score = result.score;
+            } else {
+                var result = minimax(newBoard, bot);
+                move.score = result.score;
+            }
+
+            newBoard[availSpots[i]] = move.index;
+            moves.push(move);
+        }
+
+        var bestMove;
+        if (curPlayer === bot) {
+            var bestScore = -10000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            var bestScore = 10000;
+            for (let i = 0; i < moves.length; i++) {
+                if (moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+
+        return moves[bestMove];
     }
 }
